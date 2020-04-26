43f80e3f069dac58f984492acddeff3d3ede6334
mphuttu <mpo@mbnet.fi>
2020-04-27 00:27:14 +0300

    Add files via upload
    Tehtävä 2. Etsi listan viimeistä alkiota edeltävä elementti.

---
+++ 02-teht.hs
@@ -0,0 +1,17 @@
+import Data.String
+main :: IO()
+main = do
+    putStrLn "Tehtävä 2 - Etsi listan viimeisin elementti\n"
+    print "Anna sanalista (vähhintään 2 sanaa) erotuksena väli >"
+    sanalista <- getLine
+    -- sanaLista tyyppiä IO String muutetaan wordList-sanalistaksi tyyppiä String
+    -- words -apufunktion avulla
+    let wordList = words sanalista
+    -- listan koko
+    let listLength = length wordList
+    let secondLastIndex = listLength - 2
+    print ("Annoit sanaluettelon " ++ show wordList)
+    -- listan viimeinen alkio
+    let wordLast = wordList !! secondLastIndex    
+    print ("Listan toiseksi viimeinen alkio on: " ++ show wordLast)
+    putStrLn "\nTehtävä 2 - Ratkaisun loppu - Mika Huttunen"
