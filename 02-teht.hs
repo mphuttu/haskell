ca7ef358f46f946478fcf3982bca3750a7b7a657
mphuttu <mpo@mbnet.fi>
2020-04-27 00:31:12 +0300

    Add files via upload

--- 02-teht.hs
+++ 02-teht.hs
@@ -1,7 +1,7 @@
 import Data.String
 main :: IO()
 main = do
-    putStrLn "Tehtävä 2 - Etsi listan viimeisin elementti\n"
+    putStrLn "Tehtävä 2 - Etsi listan toiseksi viimeisin elementti\n"
     print "Anna sanalista (vähhintään 2 sanaa) erotuksena väli >"
     sanalista <- getLine
     -- sanaLista tyyppiä IO String muutetaan wordList-sanalistaksi tyyppiä String
