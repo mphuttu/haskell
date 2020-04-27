94a0dfd5eb82b4a98d01e85740152cd84e1e57f4
mphuttu <mpo@mbnet.fi>
2020-04-28 01:51:57 +0300

    Add files via upload
    Tehtävä 8. Tuplaa listan elementit

---
+++ 08-teht.hs
@@ -0,0 +1,29 @@
+import Data.String
+import Data.List 
+main :: IO()
+main = do
+    putStrLn "Tehtävä 8 - Tuplaa listan sisältö\n"
+    print "Anna lista >"
+    sanalista <- getLine
+    -- sanaLista tyyppiä IO String muutetaan wordList-sanalistaksi tyyppiä String
+    -- words -apufunktion avulla
+    -- let wordList = words sanalista
+    -- let wordList = lines sanalista
+    print ("Annoit sanaluettelon: " ++ sanalista)
+    -- tuplataan alkiot
+    putStrLn "Tuplattu sanaluettelo on"
+    let doubledList = map (repeatMine 2) sanalista
+    let doubledListFinal = intercalate "" doubledList
+    putStrLn $ doubledListFinal
+        
+    putStrLn "\nTehtävä 8 - Ratkaisun loppu - Mika Huttunen"
+
+
+-- toista alkiota n kertaa
+repeatMine :: Int -> a -> [a]
+repeatMine n x
+    | n <= 0    = []
+    | otherwise = x : repeatMine (n-1) x
+
+
+
