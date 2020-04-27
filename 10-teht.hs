ca32270bc871064c2b98b682e54bf4cc91a1cf66
mphuttu <mpo@mbnet.fi>
2020-04-28 02:35:01 +0300

    Add files via upload
    Tehtävä 10 - Poista listasta joka k:s elementti

---
+++ 10-teht.hs
@@ -0,0 +1,36 @@
+import Data.String
+import Data.List 
+main :: IO()
+main = do
+    putStrLn "Tehtävä 10 - Poista listasta joka k:s elementti\n"
+    print "Anna lista >"
+    sanalista <- getLine
+    -- sanaLista tyyppiä IO String muutetaan wordList-sanalistaksi tyyppiä String
+    -- words -apufunktion avulla
+    -- let wordList = words sanalista
+    -- let wordList = lines sanalista
+    print ("Annoit sanaluettelon: " ++ sanalista)
+    putStrLn "Kuinka monennen elementin haluat poistaa ?"
+    putStrLn "Anna luku > "
+    strLkm <- getLine
+    let nLkm = read strLkm
+    -- poistetaan joka nLkm:s alkio
+    putStrLn "Poistojen jälkeen sanaluettelo on"
+    let afterList = del_every_nth nLkm sanalista
+    -- let afterListFinal = intercalate "" afterList
+    putStrLn $ afterList
+        
+    putStrLn "\nTehtävä 10 - Ratkaisun loppu - Mika Huttunen"
+
+
+-- poista joka n:s alkio
+del_every_nth :: Int -> [a] -> [a]
+del_every_nth n = concat . map init . groupMy n 
+
+-- ryhmitellään lista n:än pituiseen osaan 
+groupMy :: Int -> [a] -> [[a]] 
+groupMy n [] = []
+groupMy n xs = take n xs : groupMy n (drop n xs)
+
+
+
