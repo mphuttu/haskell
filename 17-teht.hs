482f1e646d033eb163f329a57237cfb76d395fcf
mphuttu <mpo@mbnet.fi>
2020-04-29 05:01:08 +0300

    Add files via upload
    Tehtävä 17 - Lajittele lista osalistojen pituuden mukaan, lyhin listan alkuun, jne.

---
+++ 17-teht.hs
@@ -0,0 +1,60 @@
+import Data.String
+import Data.List 
+import Control.Monad
+import Data.Function
+import Data.Ord
+import Data.Char
+main :: IO()
+main = do
+    putStrLn "Tehtävä 17 - Lajittele lista osalistojen pituuden mukaan,"
+    putStrLn "lyhin listan alkuun, jne. "
+    -- luku x
+    putStrLn "Anna  merkkijonoja. Nämä lisätään listaan."
+    putStrLn "Kun haluat lopettaa, kirjoita sana poistu!"
+    {-
+    strList <- getStrList = do
+                      line <- getLine
+                      if line == 'poistu' then return []
+                                          else fmap (line:) getStrList
+    -}
+    strList <- getUserLines
+    print("Annoit listan: " ++ show strList) 
+    putStrLn "Kun tämä lista laitetaan aakkosjärjestykseen on uusi lista: "
+    -- käytetään Data.List:in sort-funktiota
+    let sortedListA = sort strList
+    print(show sortedListA)
+    putStrLn "Kun lista järjestetään pituuden mukaan saadaan: "
+    let sortedList = sortBy lengthThenAZ strList
+    -- käytetään valmiita funktioita
+    -- print $ listStrings strList
+    print(show sortedList)
+    putStrLn "\nTehtävä 17 - Ratkaisun loppu - Mika Huttunen"
+
+
+getUserLines :: IO [String]
+getUserLines = go [""]
+   where go contents = do
+                   line <- getLine
+                   if line == "poistu"
+                      then return contents
+                      else go (line : contents)
+
+-- pituuden mukaan sitten aakkosten
+lengthThenAZ:: String -> String -> Ordering
+lengthThenAZ = comparing length `mappend` comparing (fmap toLower)
+-- lajittelufunktio, lajittelee eri tavalla
+listStrings :: [String]  -> [String]
+listStrings = reverse . sortBy sortF 
+    where
+        sortF :: String->String->Ordering
+        sortF = comparing ( length . head . words)
+-- otetaan sanoja talteen käyttäjältä
+{-
+getList :: IO String
+getList = fmap reverse (go []) where 
+    go xs = do
+        x <- getLine
+        if x == "poistu" then return xs
+                         else go (x : xs)
+
+-}
