aa2c236a9717d61782ea1b5d63f11dca6071d8dc
mphuttu <mpo@mbnet.fi>
2020-04-29 05:28:50 +0300

    Add files via upload
    Tehtävä 19. Etsi lukujen suurin yhteinen jakaja

---
+++ 19-teht.hs
@@ -0,0 +1,22 @@
+main :: IO()
+main = do
+    putStrLn "Tehtävä 19 - etsi kahden kokonaisluvun suurin "
+    putStrLn "yhteinen jakaja"   
+    -- luku a
+    putStrLn "Anna  kokonaisluku >"
+    strLuku <- getLine
+    print("Annoit luvun: " ++ strLuku) 
+    let nLuku = (read strLuku :: Integer)
+     -- luku b
+    putStrLn "Anna toinen kokonaisluku >"
+    strLukuKaks <- getLine
+    print("Annoit luvun: " ++ strLukuKaks) 
+    let nLukuKaks = (read strLukuKaks :: Integer)
+    -- lukujen suurin yhteinen jakaja
+    let nSyt = syt nLuku nLukuKaks 
+    print("Lukujen " ++ strLuku ++ " ja " ++ strLukuKaks ++ " suurin yhteinen jakaja on " ++ show nSyt)
+    putStrLn "\nTehtävä 18 - Ratkaisun loppu - Mika Huttunen"
+
+syt a b
+  | b == 0 =a 
+  | otherwise = syt b (a `mod` b)
